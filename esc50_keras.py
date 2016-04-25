9898e55e6c398411c094db50860db7f64b8fd897
Carmine Emanuele Cella <Carmine@wf82.di.ens.fr>
2016-04-25 16:39:40 +0200

    CNN working (first version)

--- esc50_keras.py
+++ esc50_keras.py
@@ -17,14 +17,18 @@ from keras.layers.core import Dense, Dropout, Activation, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 from keras.optimizers import SGD
 from keras.utils import np_utils
+from sklearn.svm import SVC
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.pipeline import make_pipeline
     
-SAMPLELEN=110272
-compute = False
+SAMPLELEN = 110272
 
-params = {'ncoeff': 20, 'fft': 4096, 'hop': 2048, \
+params = {'compute_features':False, 'compute_baseline': False, \
+          'standardize_data':True, 'augment_data': True, \
+          'ncoeff': 20, 'fft': 4096, 'hop': 2048, \
           'nclasses': 50, 'nsamples':2000, \
           'nfolds': 3, 'split':.25, \
-          'bsize': 32, 'nepoch': 2}
+          'bsize': 128, 'nepoch': 50}
 
 def compute_features (root_path, params):
     nframes = int(SAMPLELEN / params['hop']);
@@ -62,15 +66,22 @@ def create_folds (X_data, y_data, params):
         X_train, X_test = X_data[train_index], X_data[test_index]
         y_train, y_test = y_data[train_index], y_data[test_index]
     return X_train, X_test, y_train, y_test, cv
+        
+def standardize (X_data):
+    mu = np.mean (X_data, axis=0)
+    de = np.std (X_data, axis=0)
     
+    X_data = (X_data - mu) / de
     
+    mm = np.max(X_data, axis=0)
+    nn = np.min(X_data, axis=0)
+    X_data = (X_data -nn) / (mm-nn)
+    return X_data
+
 def svm_classify(X_data, y_data, cv, params):
-    from sklearn.svm import SVC
-    from sklearn.preprocessing import StandardScaler
-    from sklearn.pipeline import make_pipeline
     svm = SVC(C=1., kernel="linear")
     
-    pipeline = make_pipeline(StandardScaler(), svm)
+    pipeline = make_pipeline(svm)
 
     X_flatten = X_data.view()
     X_flatten.shape = X_flatten.shape[0], -1
@@ -78,23 +89,30 @@ def svm_classify(X_data, y_data, cv, params):
     scores = cross_val_score(pipeline, X_flatten, y_data, cv=cv, scoring="accuracy")
     return scores, cv
     
-def vgg_classify(X_train, X_test, y_train, y_test, params):
+def cnn_classify(X_train, X_test, y_train, y_test, params):
+    nb_classes = params["nclasses"]
+    batch_size = params["bsize"]
+    nb_epoch =params["nepoch"]
+    img_channels = 1
+    img_rows = X_train.shape[2]
+    img_cols = X_train.shape[3]
     
     # convert class vectors to binary class matrices
-    Y_train = np_utils.to_categorical(y_train, params["nclasses"])
-    Y_test = np_utils.to_categorical(y_test, params["nclasses"])
+    Y_train = np_utils.to_categorical(y_train, nb_classes)
+    Y_test = np_utils.to_categorical(y_test, nb_classes)
     
     model = Sequential()
     
-    model.add(Convolution2D(32, 3, 3, border_mode='same',
-                            input_shape=(1, X_train.shape[2], X_data.shape[3])))
-    model.add(Activation('relu'))
-    model.add(Convolution2D(32, 3, 3))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-    
-    model.add(Convolution2D(64, 3, 3, border_mode='same'))
+    # model.add(Convolution2D(32, 3, 3, border_mode='same',
+    #                         input_shape=(img_channels, img_rows, img_cols)))
+    # model.add(Activation('relu'))
+    # model.add(Convolution2D(32, 3, 3))
+    # model.add(Activation('relu'))
+    # model.add(MaxPooling2D(pool_size=(2, 2)))
+    # model.add(Dropout(0.25))
+    # 
+    model.add(Convolution2D(64, 3, 3, border_mode='same',
+                            input_shape=(img_channels, img_rows, img_cols)))
     model.add(Activation('relu'))
     model.add(Convolution2D(64, 3, 3))
     model.add(Activation('relu'))
@@ -105,29 +123,55 @@ def vgg_classify(X_train, X_test, y_train, y_test, params):
     model.add(Dense(512))
     model.add(Activation('relu'))
     model.add(Dropout(0.5))
-    model.add(Dense(params["nclasses"]))
+    model.add(Dense(nb_classes))
     model.add(Activation('softmax'))
     
+    model.summary ()
     # let's train the model using SGD + momentum (how original).
     sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
     model.compile(loss='categorical_crossentropy',
-                optimizer=sgd,
-                metrics=['accuracy'])
-
-    model.fit(X_train, Y_train,
-            batch_size=params["bsize"],
-            nb_epoch=params["nepoch"],
-            validation_data=(X_test, Y_test),
-            shuffle=True)
-                
+                  optimizer=sgd,
+                  metrics=['accuracy'])
+                 
+    if params["augment_data"]:
+        print ("augment data...")
+        datagen = ImageDataGenerator(
+            featurewise_center=False,  # set input mean to 0 over the dataset
+            samplewise_center=False,  # set each sample mean to 0
+            featurewise_std_normalization=False,  # divide inputs by std of the dataset
+            samplewise_std_normalization=False,  # divide each input by its std
+            zca_whitening=False,  # apply ZCA whitening
+            rotation_range=0,  # randomly rotate images in the range (degrees, 0 to 180)
+            width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
+            height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
+            horizontal_flip=True,  # randomly flip images
+            vertical_flip=False)  # randomly flip images
+    
+        # compute quantities required for featurewise normalization
+        # (std, mean, and principal components if ZCA whitening is applied)
+        datagen.fit(X_train)
+        
+        model.fit_generator(datagen.flow(X_train, Y_train,
+                            batch_size=batch_size),
+                            samples_per_epoch=X_train.shape[0],
+                            nb_epoch=nb_epoch,
+                            validation_data=(X_test, Y_test))       
+    else:
+        model.fit(X_train, Y_train,
+              batch_size=batch_size,
+              nb_epoch=nb_epoch,
+              validation_data=(X_test, Y_test),
+              shuffle=True)
+            
 if __name__ == "__main__":
     print ("ESC-50 classification with Keras");
     print ("")    
 
-    if compute == True:
+    if params["compute_features"] == True:
         print ("computing features...")
         X_data, y_data = compute_features ("../../datasets/ESC-50-master", params)
         print ("saving features...")
+        np.save ('params', params)
         np.save ('X_data', X_data)
         np.save ('y_data', y_data)
     else:
@@ -135,14 +179,22 @@ if __name__ == "__main__":
         X_data = np.load ("X_data.npy")
         y_data = np.load ("y_data.npy")    
 
+    X_data.astype('float32')
+    y_data.astype('uint8')
+    
+    if params["standardize_data"] == True:
+        print ("standardizing data...")
+        X_data = standardize(X_data)
+        
     print ("making folds...")
     X_train, X_test, y_train, y_test, cv = create_folds(X_data, y_data, params)
-        
-    print ("computing linear SVM basline...")
-    scores, cv = svm_classify(X_data, y_data, cv, params)
-    print ("scores: " + str(scores))
+
+    if params["compute_baseline"] == True:
+        print ("computing linear SVM baseline...")
+        scores, cv = svm_classify(X_data, y_data, cv, params)
+        print ("scores: " + str(scores))
     
-    print ("computing vgg CNN classification...")
-    vgg_classify(X_train, X_test, y_train, y_test, params)
+    print ("computing CNN classification...")
+    cnn_classify(X_train, X_test, y_train, y_test, params)
     
     
