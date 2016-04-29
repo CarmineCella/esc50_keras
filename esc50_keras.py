1b5c3aa1873107053675d9faa808f609bb76e682
Carmine Emanuele Cella <cella@di.ens.fr>
2016-04-29 14:42:50 +0200

    Fixed bugs for features and added cached version

--- esc50_keras.py
+++ esc50_keras.py
@@ -9,8 +9,10 @@ ESC-50 dataset classification with Keras
 
 import os
 import fnmatch
+import joblib
 import librosa
 import numpy as np
+import os.path
 from sklearn.cross_validation import StratifiedShuffleSplit
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Activation, Flatten
@@ -27,10 +29,9 @@ from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
 
 class ShapeWrapper(BaseEstimator):
-    
     def __init__(self, estimator, to_shape=(-1,)):
-        self.estimator = estimator
         self.to_shape = to_shape
+        self.estimator = estimator
         
     def _transform(self, X):
         Xreshaped = X.view()
@@ -42,7 +43,7 @@ class ShapeWrapper(BaseEstimator):
         Xreshaped = self._transform(X)
         self.estimator.fit(Xreshaped, y)
         return self
-    
+        
     def predict(self, X):
         Xreshaped = self._transform(X)
         return self.estimator.predict(Xreshaped)
@@ -50,52 +51,80 @@ class ShapeWrapper(BaseEstimator):
     def transform(self, X, y=None):
         Xreshaped = self._transform(X)
         return self.estimator.transform(Xreshaped)
-        
-SAMPLELEN = 110272
 
-params = {'compute_features':True, 'plot':True, \
-          'compute_baseline': False, 'compute_cnn': True, \
+SAMPLELEN = 110250 # in samples is 5 sec @ 22050
+
+params = {'plot':True, 'features':'mfcc', \
+          'compute_baseline': True, 'compute_cnn': True, \
           'standardize_data':True, 'augment_data': True, \
-          'ncoeff': 20, 'fft': 2048, 'hop': 1024, \
+          'ncoeff': 20, 'hop': 2048, \
           'nclasses': 50, 'nsamples':2000, \
           'nfolds': 1, 'split':.2, \
           'bsize': 128, 'nepoch': 200}
 
+def get_features (file, features, hop, bins):
+    y = np.zeros(SAMPLELEN);   
+    yt, sr = librosa.core.load  (file, mono=True)
+    
+    if len(yt) == 0: 
+        print ('*** warning: empty file -> ' + file + '! ***')
+        return 0
+
+    min_length = min(len(y), len(yt))
+    y[:min_length] = yt[:min_length]
+    
+    if features == 'mfcc':
+        C = librosa.feature.mfcc(y=y, sr=sr, hop_length=params['hop'],n_mfcc = params['ncoeff'])    
+    elif features == 'cqt':
+        C = np.log1p(1000 * np.abs (librosa.core.cqt( y=y, sr=sr, hop_length=hop, n_bins=bins, real=False)))    
+    else:
+        print('unsupported features')
+        return 0
+    return C
+
+cachedir = os.path.expanduser('~/esc50_joblib')
+memory = joblib.Memory(cachedir=cachedir, verbose=1)
+cached_get_features = memory.cache(get_features)
+
 def compute_features (root_path, params):
-    nframes = int(SAMPLELEN / params['hop']);
+    hop = params['hop']
+    bins = params['ncoeff']
+    features = params['features']
         
-    X_data = np.zeros ((params['nsamples'], 2, params['ncoeff'], nframes))
     y_data = np.zeros ((params['nsamples']))
     
     classes = 0
     samples = 0
+    
+    X_list = []
     for root, dir, files in os.walk(root_path):
-        print ("class: " + root.split("/")[-1])
         waves = fnmatch.filter(files, "*.wav")
-
         if len(waves) != 0:
-            for items in waves:
-                print ("\tsample: " + items)
-
-                y, sr = librosa.core.load(root + '/' + items)
-                if len(y) == 0: 
-                    print ('empty file!')
-                    continue
-
-                #C = librosa.feature.mfcc(y=y, sr=sr, hop_length=params['hop'],n_mfcc = params['ncoeff'])
-                C = np.abs (librosa.core.cqt( y=y, sr=sr, hop_length=params['hop'], real=False))                
-                C = C[:params['ncoeff'],:nframes]
-                X_data[samples, 0, :C.shape[0], :C.shape[1]] = C
+            print ("class: " + root.split("/")[-1])
+            X_list.append([
+                cached_get_features(os.path.join(root, item), features, hop, bins)
+                for item in waves]) 
+            for item in waves:
                 y_data[samples] = classes
                 samples = samples + 1
-            classes = classes + 1            
-        print ("")
-            
+            classes = classes + 1
+    
+    X_flat_list = [X_list[class_id][file_id]
+                for class_id in range(len(X_list))
+                for file_id in range(len(X_list[class_id]))]
+                    
+    X_data = np.stack(X_flat_list, axis=2)
+    X_data = np.transpose(X_data, (2,0,1))
+    d1 = X_data.shape[0]
+    d2 = X_data.shape[1]
+    d3 = X_data.shape[2]
+    X_data = np.reshape(X_data, (d1,1,d2,d3))
+    
     print ("classes = " + str (classes))
     print ("samples = " + str (samples))
 
     return X_data, y_data
-    
+
 def create_folds (X_data, y_data, params):    
     cv = StratifiedShuffleSplit(y_data, n_iter=params['nfolds'], test_size=params['split'])
     return cv
@@ -104,15 +133,20 @@ def standardize (X_train, X_test):
     mu = np.mean (X_train, axis=0)
     de = np.std (X_train, axis=0)
     
-    X_train = (X_train - mu) / de
-    X_test = (X_test - mu) / de
+    eps = np.finfo('float32').eps
+    X_train = (X_train - mu) / (eps + de)
+    X_test = (X_test - mu) / (eps + de)
     return X_train, X_test
 
 def svm_classify(X_train, X_test, y_train, y_test, params):
-    svm = SVC(C=1., kernel="linear")
-    rsvm = ShapeWrapper(svm)
-    rsvm.fit(X_train, y_train)
-    y_pred = rsvm.predict(X_test)
+    svm = SVC(C=1.)
+    #rsvm = ShapeWrapper(svm)
+    X_svm_train = X_train.view ()
+    X_svm_train = np.reshape (X_svm_train, (X_svm_train.shape[0], -1))    
+    X_svm_test = X_test.view ()
+    X_svm_test = np.reshape (X_svm_test, (X_svm_test.shape[0], -1))  
+    svm.fit(X_svm_train, y_train)
+    y_pred = svm.predict(X_svm_test)
     cm = confusion_matrix(y_test, y_pred)
     score = accuracy_score(y_test, y_pred)
 
@@ -187,7 +221,7 @@ def cnn_classify(X_train, X_test, y_train, y_test, params):
         # compute quantities required for featurewise normalization
         # (std, mean, and principal components if ZCA whitening is applied)
         datagen.fit(X_train)
-        
+#    
         model.fit_generator(datagen.flow(X_train, Y_train,
                             batch_size=batch_size),
                             samples_per_epoch=X_train.shape[0],
@@ -219,21 +253,13 @@ if __name__ == "__main__":
     print ("ESC-50 classification with Keras");
     print ("")    
 
-    if params["compute_features"] == True:
-        print ("computing features...")
-        X_data, y_data = compute_features ("../../datasets/ESC-50-master", params)
-        print ("saving features...")
-        np.save ('params', params)
-        np.save ('X_data', X_data)
-        np.save ('y_data', y_data)
-    else:
-        print ("reloading features...")
-        X_data = np.load ("X_data.npy")
-        y_data = np.load ("y_data.npy")    
+    print ("computing features...")
+    X_data, y_data = compute_features ("../../datasets/ESC-50-master", params)
 
     X_data.astype('float32')
     y_data.astype('uint8')
-        
+
+     
     print ("making folds...")
     cv = create_folds(X_data, y_data, params)
         
@@ -246,10 +272,18 @@ if __name__ == "__main__":
         if params["standardize_data"] == True:
             print ("standardizing data...")
             X_train, X_test = standardize(X_train, X_test)
+            
+        if params['plot'] == True:
+            X_flat = np.transpose(X_train, (0, 3, 1, 2))
+            X_flat = np.reshape(X_flat, (X_flat.shape[0]*X_flat.shape[1], X_flat.shape[3]))
+            X_corr = np.dot (X_flat.T, X_flat)
         
-        
+            plt.imshow (X_corr)
+            plt.title ('Correlation matrix')
+            plt.show ()
+
         if params["compute_baseline"] == True:
-            print ("computing linear SVM baseline...")
+            print ("computing SVM baseline...")
             score, cm = svm_classify(X_train, X_test, y_train, y_test, params)
             print ("score " + str(score))
             
